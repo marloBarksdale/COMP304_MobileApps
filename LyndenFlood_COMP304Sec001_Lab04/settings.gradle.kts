ca1c8c2ebb382740f3feb4cecfbf918f65b75f70
marloBarksdale <lyndenflood@gmail.com>
2025-07-23 01:26:06 -0400

    first

---
+++ LyndenFlood_COMP304Sec001_Lab04/settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "LyndenFlood_COMP304Sec001_Lab04"
+include(":app")
+ 
